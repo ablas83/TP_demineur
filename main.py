568243cf0460c73c084e9fc9132785154ed8a51d
Alexis Blas <ablas@diginamic-formation.fr>
2023-06-20 15:52:32 +0200

    Question 3 parser et message d' ouverture case et de flag

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import sys
-hauteur_grille = int (sys.argv [1])
-largeur_grille = int (sys.argv [2])
+hauteur_grille = int(sys.argv[1])
+largeur_grille = int(sys.argv[2])
 while True:
     coordinput = input("veuillez choisir x et y")
+    coord = coordinput.rsplit(" ")
+    if len(coord) == 2 and int(coord[0]) and int(coord[1]):
+        print("Ouvrir la case", coord[0], coord[1])
+    elif len(coord) == 3 and coord[0] == "F" and int(coord[1]) and int(coord[2]):
+        print("Flagger la case", coord[1], coord[2])
+    else:
+        raise ValueError("syntaxe invalide")
