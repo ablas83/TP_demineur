f8483b25a9bca4a359b1894e178de8a96fe471a2
Alexis Blas <ablas@diginamic-formation.fr>
2023-06-20 16:01:15 +0200

    Question 4 Création classe MineSweeper

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import sys
 hauteur_grille = int(sys.argv[1])
 largeur_grille = int(sys.argv[2])
+
+class MineSweeper ():
+    def open(self, x, y):
+        print("Ouvrir la case", x, y)
+
+    def flag(self, x, y):
+        print("Flagger la case", x, y)
+ms = MineSweeper()
 while True:
-    coordinput = input("veuillez choisir x et y")
+    coordinput = input("veuillez choisir 'x y' ou 'F x y' pour mettre un flag")
     coord = coordinput.rsplit(" ")
     if len(coord) == 2 and int(coord[0]) and int(coord[1]):
-        print("Ouvrir la case", coord[0], coord[1])
+        ms.open(int(coord[0]), int(coord[1]))
     elif len(coord) == 3 and coord[0] == "F" and int(coord[1]) and int(coord[2]):
-        print("Flagger la case", coord[1], coord[2])
+        ms.flag(int(coord[1]), int(coord[2]))
     else:
         raise ValueError("syntaxe invalide")
+
