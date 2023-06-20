357b85d013f2bcdf223c6a7240588ec980a65998
Alexis Blas <ablas@diginamic-formation.fr>
2023-06-20 16:34:12 +0200

    Question 6 Création option quit et newgame pour l'utilisateur

--- main.py
+++ main.py
@@ -23,12 +23,16 @@ class MineSweeper ():
 ms = MineSweeper()
 ms.newgame()
 while True:
-    coordinput = input("veuillez choisir 'x y' ou 'F x y' pour mettre un flag")
+    coordinput = input("veuillez choisir 'x y' ou 'F x y' pour mettre un flag ou quit ou newgame")
     coord = coordinput.rsplit(" ")
     if len(coord) == 2 and int(coord[0]) and int(coord[1]):
         ms.open(int(coord[0]), int(coord[1]))
     elif len(coord) == 3 and coord[0] == "F" and int(coord[1]) and int(coord[2]):
         ms.flag(int(coord[1]), int(coord[2]))
+    elif (coordinput=="quit"):
+        break
+    elif(coordinput=="newgame"):
+        ms.newgame(hauteur_grille,largeur_grille)
     else:
         raise ValueError("syntaxe invalide")
 
