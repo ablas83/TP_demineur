5dda3f1a1830d1fe376b485622a395c12550d0a8
Alexis Blas <ablas@diginamic-formation.fr>
2023-06-20 16:43:38 +0200

    Question 7 ne pas avoir d'exception lorsque la prtie n'est pas en cours et possibilité de pouvoir en lancer une avec newgame

--- main.py
+++ main.py
@@ -21,14 +21,20 @@ class MineSweeper ():
         self.is_playing = True
 
 ms = MineSweeper()
-ms.newgame()
+
 while True:
     coordinput = input("veuillez choisir 'x y' ou 'F x y' pour mettre un flag ou quit ou newgame")
     coord = coordinput.rsplit(" ")
     if len(coord) == 2 and int(coord[0]) and int(coord[1]):
-        ms.open(int(coord[0]), int(coord[1]))
+        try:
+            ms.open(int(coord[0]), int(coord[1]))
+        except Exception as e:
+            print("La partie n'est pas en cours")
     elif len(coord) == 3 and coord[0] == "F" and int(coord[1]) and int(coord[2]):
-        ms.flag(int(coord[1]), int(coord[2]))
+        try:
+            ms.flag(int(coord[1]), int(coord[2]))
+        except Exception as e:
+            print("La partie n'est pas en cours")
     elif (coordinput=="quit"):
         break
     elif(coordinput=="newgame"):
